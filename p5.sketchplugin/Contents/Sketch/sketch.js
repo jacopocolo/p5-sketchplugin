edc980cf31b808b6f6714c76063300032bf995b7
Jacopo Colò <jacopo.colo@designit.com>
2016-01-05 09:21:18 +0100

    made a default sketch.js file

--- p5.sketchplugin/Contents/Sketch/sketch.js
+++ p5.sketchplugin/Contents/Sketch/sketch.js
@@ -1,10 +1,7 @@
-function setup() {
-    createCanvas(500, 500);
-    background("#e6e6e6");
-  }
+setup() {
+createCanvas(500,500)
+}
 
-  function draw() {
-    fill("#0000ff")
-    line(0,0,100,100)
-    bezier(10, 10, 10, 10, 90, 45, 45, 90);
+draw() {
+line(0,0,100,100)
 }
