15c580f2e9cfb53a90c78231debc3c922722bcca
Jacopo Colò <jacopocolo@gmail.com>
2016-01-15 21:54:47 +0100

    added noise()

--- p5.sketchplugin/Contents/Sketch/sketch.js
+++ p5.sketchplugin/Contents/Sketch/sketch.js
@@ -1,17 +1,16 @@
 var angles = [ 30, 10, 45, 35, 60, 38, 75, 67 ];
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(400, 400);
 }
+
+var noiseScale=0.02;
 
 function draw() {
-  pieChart(300, angles);
-}
-
-function pieChart(diameter, data) {
-  var lastAngle = 0;
-  for (var i = 0; i < data.length; i++) {
-    arc(width/2, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
-    lastAngle += radians(angles[i]);
+  background("#000000");
+  for (var x=0; x < width; x++) {
+    var noiseVal = noise((100+x)*noiseScale, 200*noiseScale);
+    stroke("rgb("+noiseVal*255+","+noiseVal*255+","+noiseVal*255+")");
+    line(x, 200+noiseVal*80, x, height);
   }
 }
--- p5.sketchplugin/Contents/Sketch/utils.js
+++ p5.sketchplugin/Contents/Sketch/utils.js
@@ -189,6 +189,84 @@ function map(n, start1, stop1, start2, stop2) {
   return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
   };
 
+  var PERLIN_YWRAPB = 4;
+  var PERLIN_YWRAP = 1<<PERLIN_YWRAPB;
+  var PERLIN_ZWRAPB = 8;
+  var PERLIN_ZWRAP = 1<<PERLIN_ZWRAPB;
+  var PERLIN_SIZE = 4095;
+
+  var perlin_octaves = 4; // default to medium smooth
+  var perlin_amp_falloff = 0.5; // 50% reduction/octave
+
+  var scaled_cosine = function(i) {
+    return 0.5*(1.0-Math.cos(i*Math.PI));
+  };
+
+  var perlin; // will be initialized lazily by noise() or noiseSeed()
+
+  function noise(x,y,z) {
+      y = y || 0;
+      z = z || 0;
+
+      if (perlin == null) {
+        perlin = new Array(PERLIN_SIZE + 1);
+        for (var i = 0; i < PERLIN_SIZE + 1; i++) {
+          perlin[i] = Math.random();
+        }
+      }
+
+      if (x<0) { x=-x; }
+      if (y<0) { y=-y; }
+      if (z<0) { z=-z; }
+
+      var xi=Math.floor(x), yi=Math.floor(y), zi=Math.floor(z);
+      var xf = x - xi;
+      var yf = y - yi;
+      var zf = z - zi;
+      var rxf, ryf;
+
+      var r=0;
+      var ampl=0.5;
+
+      var n1,n2,n3;
+
+      for (var o=0; o<perlin_octaves; o++) {
+        var of=xi+(yi<<PERLIN_YWRAPB)+(zi<<PERLIN_ZWRAPB);
+
+        rxf = scaled_cosine(xf);
+        ryf = scaled_cosine(yf);
+
+        n1  = perlin[of&PERLIN_SIZE];
+        n1 += rxf*(perlin[(of+1)&PERLIN_SIZE]-n1);
+        n2  = perlin[(of+PERLIN_YWRAP)&PERLIN_SIZE];
+        n2 += rxf*(perlin[(of+PERLIN_YWRAP+1)&PERLIN_SIZE]-n2);
+        n1 += ryf*(n2-n1);
+
+        of += PERLIN_ZWRAP;
+        n2  = perlin[of&PERLIN_SIZE];
+        n2 += rxf*(perlin[(of+1)&PERLIN_SIZE]-n2);
+        n3  = perlin[(of+PERLIN_YWRAP)&PERLIN_SIZE];
+        n3 += rxf*(perlin[(of+PERLIN_YWRAP+1)&PERLIN_SIZE]-n3);
+        n2 += ryf*(n3-n2);
+
+        n1 += scaled_cosine(zf)*(n2-n1);
+
+        r += n1*ampl;
+        ampl *= perlin_amp_falloff;
+        xi<<=1;
+        xf*=2;
+        yi<<=1;
+        yf*=2;
+        zi<<=1;
+        zf*=2;
+
+        if (xf>=1.0) { xi++; xf--; }
+        if (yf>=1.0) { yi++; yf--; }
+        if (zf>=1.0) { zi++; zf--; }
+      }
+      return r;
+    };
+
 function resizeLayerToFitText(layer) {
 	[layer adjustFrameToFit];
     [layer select:true byExpandingSelection:false];
